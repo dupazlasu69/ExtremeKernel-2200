335255505b8ceb96e30dae4a240990b9fff40348
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-09-14 17:20:16 +0300

    Revert "build: use Neutron Clang 18"
    This reverts commit fda91f6f410d3670596850a1953c166594727c91.

--- build.sh
+++ build.sh
@@ -42,32 +42,16 @@ pushd $(dirname "$0") > /dev/null
 CORES=`cat /proc/cpuinfo | grep -c processor`
 
 # Define toolchain variables
-CLANG_DIR=$PWD/toolchain/neutron_18
-PATH=$CLANG_DIR/bin:$CLANG_DIR/lib:$PATH
-
-# Check if toolchain exists
-if [ ! -f "$CLANG_DIR/bin/clang-18" ]; then
-    echo "-----------------------------------------------"
-    echo "Toolchain not found! Downloading..."
-    echo "-----------------------------------------------"
-    rm -rf $CLANG_DIR
-    mkdir -p $CLANG_DIR
-    pushd toolchain/neutron_18 > /dev/null
-    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024
-    echo "-----------------------------------------------"
-    echo "Patching toolchain..."
-    echo "-----------------------------------------------"
-    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
-    echo "-----------------------------------------------"
-    echo "Cleaning up..."
-    popd > /dev/null
-fi
+CLANG_DIR=$PWD/toolchain/clang-r416183b
+GCC_DIR=$PWD/toolchain/gcc_4.9
+PATH=$CLANG_DIR/bin:$CLANG_DIR/lib:$GCC_DIR/bin:$GCC_DIR/lib:$PATH
 
 MAKE_ARGS="
 LLVM=1 \
 LLVM_IAS=1 \
 ARCH=arm64 \
 READELF=$CLANG_DIR/bin/llvm-readelf \
+CROSS_COMPILE=$GCC_DIR/bin/aarch64-linux-gnu- \
 O=out
 "
 
